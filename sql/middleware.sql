c781df7e77d586c75f05d0054e81f4ea4ab1d945
Patricio Soto <psoto@napsis.cl>
2017-07-22 07:42:06 -0400

    unique

--- sql/middleware.sql
+++ sql/middleware.sql
@@ -12,5 +12,6 @@ CREATE TABLE `user` (
   `username` varchar(128) DEFAULT NULL,
   `password` varchar(96) DEFAULT NULL,
   `name` varchar(1045) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `_username_unq` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
